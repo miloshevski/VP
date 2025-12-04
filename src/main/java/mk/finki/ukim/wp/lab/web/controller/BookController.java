53049fb72b88c277c7912223344451654751f2b4
Aleksandar <milosevskialeksandar18@gmail.com>
2025-12-04 16:53:12 +0100

    delete servlet

--- src/main/java/mk/finki/ukim/wp/lab/web/controller/BookController.java
+++ src/main/java/mk/finki/ukim/wp/lab/web/controller/BookController.java
@@ -8,7 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("/books")
+@RequestMapping("/")
 public class BookController {
 
     private final BookService bookService;
